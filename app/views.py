55bec8bd4370d4b95a89b6a2945f10b64258f401
Ryyyu-9 <ryyyu.trr@gmail.com>
2023-12-27 00:02:42 +0900

    add 2 pages

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from .models import Task
+
 # Create your views here.
+def index(request):
+    return render(request, 'app/index.html')
+
+def dashboard(request):
+    tasks = Task.objects.order_by('deadline_date')
+    context = {'tasks': tasks}
+    return render(request, 'app/dashboard.html', context)
+
