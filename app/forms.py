7f4f6748b9caad1429185480fd99e6f5a400b548
Ryyyu-9 <ryyyu.trr@gmail.com>
2024-01-03 00:04:10 +0900

    enable to add tasks

--- app/forms.py
+++ app/forms.py
@@ -2,14 +2,13 @@ from django import forms
 
 from .models import Task
 
-class TaskForm(forms.Form):
-    # class Meta:
-    #     model = Task
-    #     # fields = ['task_name']
-    #     # labels = {'task_name': 'Task Name'}
-    #     # widgets = {'task_detail': forms.Textarea(attrs={'cols':80})}
-    #     # labels = {'task_detail': 'Task detail'}
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['task_name']
+
     task_name = forms.CharField(max_length=100)
     task_detail = forms.CharField(max_length=1000)
-    parent_task_id = forms.IntegerField()
-    cost_time = forms.TimeField()
+    parent_task_id = forms.IntegerField(required=False)
+    cost_time = forms.IntegerField(required=True)
+    deadline_date = forms.DateTimeField(required=True)
--- app/models.py
+++ app/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    task_name = models.CharField(max_length=50)
-    task_detail = models.CharField(max_length=500)
-    parent_task_id = models.IntegerField(max_length=10)
-    cost_time = models.TimeField()
-    deadline_date = models.DateField()
-    register_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now_add=True)
-    is_deleted = models.BooleanField()
+    task_name = models.CharField(max_length=50, null=False)
+    task_detail = models.CharField(max_length=500, null=False)
+    parent_task_id = models.IntegerField(max_length=10, null=True, blank=True)
+    cost_time = models.IntegerField(null=True)
+    deadline_date = models.DateField(null=True)
+    register_date = models.DateTimeField(auto_now_add=True, null=False)
+    update_date = models.DateTimeField(auto_now_add=True, null=False)
+    is_deleted = models.BooleanField(null=True)
 
     def __str__(self) -> str:
         """task_nameを返す"""
